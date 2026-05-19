ef1a72f8c4531e73cb3bc82ef1be3f4dbcd0b138
soumyadeep70 <soumyadeepdash70@gmail.com>
2026-05-19 07:57:12 +0530

    setup initial project

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Flake dependency pinning for SpectraEQ";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs =
+    inputs@ { flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              llvmPackages_22.clang
+              llvmPackages_22.clang-tools
+              llvmPackages_22.lldb
+              
+              cmake
+              ninja
+              gnumake
+
+              valgrind
+              perf
+
+              cppcheck
+              pkg-config
+              catch2_3
+              
+              nixd
+            ];
+          };
+        };
+    };
+}
